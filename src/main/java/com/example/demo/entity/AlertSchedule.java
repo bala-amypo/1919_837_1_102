d012724fd39d7973ab1457a67484a4b720e72878
bala-amypo <bala@amypo.in>
2025-12-20 17:19:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertSchedule.java
+++ src/main/java/com/example/demo/entity/AlertSchedule.java
@@ -1,53 +1,26 @@
 package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
-public class AlertSchedule{
+@Table(name = "alert_schedules")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AlertSchedule {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String warranty;
-    private Long daysBeforeExpiry;
+
+    @ManyToOne
+    @JoinColumn(name = "warranty_id", nullable = false)
+    private Warranty warranty;
+
+    private Integer daysBeforeExpiry;
+
     private Boolean enabled;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getWarranty() {
-        return warranty;
-    }
-
-    public void setWarranty(String warranty) {
-        this.warranty = warranty;
-    }
-
-    public Long getDaysBeforeExpiry() {
-        return daysBeforeExpiry;
-    }
-
-    public void setDaysBeforeExpiry(Long daysBeforeExpiry) {
-        this.daysBeforeExpiry = daysBeforeExpiry;
-    }
-
-    public Boolean getEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    public AlertSchedule(Long id, String warranty, Long daysBeforeExpiry, Boolean enabled){
-        this.id=id;
-        this.warranty=warranty;
-        this.daysBeforeExpiry=daysBeforeExpiry;
-        this.enabled=enabled;
-    }
-    public AlertSchedule(){
-        
-    }
 }
