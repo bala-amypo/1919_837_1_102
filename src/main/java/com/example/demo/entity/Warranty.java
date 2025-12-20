d17bd3e7be83be955754c6e5b2b29ee1fd0ee200
bala-amypo <bala@amypo.in>
2025-12-20 17:04:27 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warranty.java
+++ src/main/java/com/example/demo/entity/Warranty.java
@@ -1,77 +1,35 @@
 package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDate;
-import jakarta.persistence.Column;
 
 @Entity
-public class Warranty{
+@Table(name = "warranties")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Warranty {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String user;
-    private String product;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
     private LocalDate purchaseDate;
+
     private LocalDate expiryDate;
-    @Column(unique=true)
+
+    @Column(unique = true, nullable = false)
     private String serialNumber;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUser() {
-        return user;
-    }
-
-    public void setUser(String user) {
-        this.user = user;
-    }
-
-    public String getProduct() {
-        return product;
-    }
-
-    public void setProduct(String product) {
-        this.product = product;
-    }
-
-    public LocalDate getPurchaseDate() {
-        return purchaseDate;
-    }
-
-    public void setPurchaseDate(LocalDate purchaseDate) {
-        this.purchaseDate = purchaseDate;
-    }
-
-    public LocalDate getExpiryDate() {
-        return expiryDate;
-    }
-
-    public void setExpiryDate(LocalDate expiryDate) {
-        this.expiryDate = expiryDate;
-    }
-
-    public String getSerialNumber() {
-        return serialNumber;
-    }
-
-    public void setSerialNumber(String serialNumber) {
-        this.serialNumber = serialNumber;
-    }
-
-    public Warranty(Long id, String user, String product, LocalDate purchaseDate, LocalDate expiryDate, String serialNumber){
-        this.id=id;
-        this.user = user;
-        this.product = product;
-        this.purchaseDate = purchaseDate;
-        this.expiryDate = expiryDate;
-        this.serialNumber=serialNumber;
-    }
-    public Warranty(){
-        
-    }
 }
