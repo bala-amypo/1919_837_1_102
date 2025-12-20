5c5dd4791f1b1226f8ea07c488a4f177af6c898f
bala-amypo <bala@amypo.in>
2025-12-20 17:16:28 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,62 +1,27 @@
 package com.example.demo.entity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
+import lombok.*;
+
 @Entity
-public class Product{
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+
     private String brand;
+
+    @Column(nullable = false)
     private String modelNumber;
+
     private String category;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public String getModelNumber() {
-        return modelNumber;
-    }
-
-    public void setModelNumber(String modelNumber) {
-        this.modelNumber = modelNumber;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-    public Product(Long id, String name, String brand, String modelNumber, String category){
-        this.id=id;
-        this.name=name;
-        this.brand=brand;
-        this.modelNumber=modelNumber;
-        this.category=category;
-    }
-    public Product(){
-        
-    }
 }
