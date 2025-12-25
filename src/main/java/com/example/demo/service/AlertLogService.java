f745ce9fe8a5a375a13dc72a4bd263713596ea5b
bala-amypo <bala@amypo.in>
2025-12-25 15:37:34 +0000

    User commit

--- src/main/java/com/example/demo/service/AlertLogService.java
+++ src/main/java/com/example/demo/service/AlertLogService.java
@@ -7,3 +7,4 @@ public interface AlertLogService {
     AlertLog addLog(Long warrantyId, String message);
     List<AlertLog> getLogs(Long warrantyId);
 }
+
--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -7,3 +7,4 @@ public interface ProductService {
     Product addProduct(Product product);
     List<Product> getAllProducts();
 }
+
