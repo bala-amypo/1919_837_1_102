aee840e7addf8768fe82553d3db88d3b965235f0
bala-amypo <bala@amypo.in>
2025-12-25 05:56:31 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warranty.java
+++ src/main/java/com/example/demo/entity/Warranty.java
@@ -1,45 +1,34 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+
 import java.time.LocalDate;
 
 @Entity
+@Table(name = "warranties")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Warranty {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String serialNumber;
+
+    private LocalDate purchaseDate;
+
+    private LocalDate expiryDate;
+
     @ManyToOne
+    @JoinColumn(name = "user_id")
     private User user;
 
     @ManyToOne
+    @JoinColumn(name = "product_id")
     private Product product;
-
-    private LocalDate purchaseDate;
-    private LocalDate expiryDate;
-    private String serialNumber;
-
-    public Warranty() {}
-
-    public Warranty(Long id, User user, Product product,
-                    LocalDate purchaseDate, LocalDate expiryDate,
-                    String serialNumber) {
-        this.id = id;
-        this.user = user;
-        this.product = product;
-        this.purchaseDate = purchaseDate;
-        this.expiryDate = expiryDate;
-        this.serialNumber = serialNumber;
-    }
-
-    public Long getId() { return id; }
-    public User getUser() { return user; }
-    public Product getProduct() { return product; }
-    public LocalDate getPurchaseDate() { return purchaseDate; }
-    public LocalDate getExpiryDate() { return expiryDate; }
-    public String getSerialNumber() { return serialNumber; }
-
-    public void setUser(User user) { this.user = user; }
-    public void setProduct(Product product) { this.product = product; }
 }
