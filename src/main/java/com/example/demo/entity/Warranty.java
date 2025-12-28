0d63c6c41e4d3c0a582d791b61f49090ab937bbc
bala-amypo <bala@amypo.in>
2025-12-28 17:13:16 +0000

    User commit

--- src/main/java/com/example/demo/entity/Warranty.java
+++ src/main/java/com/example/demo/entity/Warranty.java
@@ -7,23 +7,32 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Entity
+@Table(name = "warranties")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Warranty {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDate purchaseDate;
+    @ManyToOne
+    private User user;
 
-    @Column(name = "end_date")
+    @ManyToOne
+    private Product product;
+
+    private LocalDate purchaseDate;
     private LocalDate expiryDate;
 
     @Column(unique = true)
     private String serialNumber;
 
-    @ManyToOne
-    private Product product;
+    @OneToMany(mappedBy = "warranty", cascade = CascadeType.ALL)
+    private List<AlertSchedule> schedules;
 
-    @ManyToOne
-    private User user;
+    @OneToMany(mappedBy = "warranty", cascade = CascadeType.ALL)
+    private List<AlertLog> logs;
 }
